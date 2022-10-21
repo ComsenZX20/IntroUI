6e8dd364611cfdb0df606fd26dccb009522a0260
ComsenZX2O <>
2022-10-21 12:33:59 +0100

    menuanimation

--- Intro Interfaz de Usuario/Assets/scripts/introanim.cs
+++ Intro Interfaz de Usuario/Assets/scripts/introanim.cs
@@ -11,17 +11,27 @@ public class introanim : MonoBehaviour
     {
         //para ocultar logo
 
-        LeanTween.moveLocalY(logo, -300f, 0.0f);
+        LeanTween.moveLocalY(logo, -350f, 0.0f);
         //para hacer aparecer el logo
-        LeanTween.moveLocalY(logo, 4f, 0.0f);
+        LeanTween.moveLocalY(logo, 4f, 1.0f).setEaseInElastic().setOnComplete(Latido);
         //para moverlo localmente entre ambos ejes
         //LeanTween.moveLocal(logo, new Vector3(670f,72f,0f), 1.5f)
-        LeanTween.moveLocal(logo, new Vector3(15f, 0f, 0f), 1.5f);
+        // LeanTween.moveLocal(logo, new Vector3(670f, 72f, 0f), 1.5f);
     }
 
     // Update is called once per frame
-    void Update()
+    void Latido()
     {
-        
+        LeanTween.scale(logo, Vector3.one, 0.25f).setEaseOutBack().setOnComplete(() =>
+        {
+            LeanTween.alphaCanvas(gameObject.GetComponent<CanvasGroup>(), 0.0f, 0.75f).setOnComplete(Desactivar);
+        });
+
+    }
+    void Desactivar()
+    {
+
+        gameObject.SetActive(false);
+
     }
 }
