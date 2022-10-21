64fb2254bd5959a892e79b658ad275a204cad61e
ComsenZX2O <>
2022-10-21 12:09:52 +0100

    Update introanim.cs

--- Intro Interfaz de Usuario/Assets/scripts/introanim.cs
+++ Intro Interfaz de Usuario/Assets/scripts/introanim.cs
@@ -16,7 +16,7 @@ public class introanim : MonoBehaviour
         LeanTween.moveLocalY(logo, 4f, 0.0f);
         //para moverlo localmente entre ambos ejes
         //LeanTween.moveLocal(logo, new Vector3(670f,72f,0f), 1.5f)
-        LeanTween.moveLocal(logo, new Vector3(670f, 72f, 0f), 1.5f);
+        LeanTween.moveLocal(logo, new Vector3(15f, 0f, 0f), 1.5f);
     }
 
     // Update is called once per frame
